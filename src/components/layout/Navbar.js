50411120bec2f21ed7d483fbc75f700f75938951
elmentrix <adilsheik98@gmail.com>
2018-10-14 19:55:46 +0530

    Fixed extra margin in nav links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,9 +8,9 @@ const Navbar = () => {
             <div className="container">
                 <NavLink to='/glugpace/' id="logo">GLUG PACE</NavLink>
                 <ul className="navbar-links">
-                    <li><NavLink to='/glugpace/events'>Events</NavLink></li>
-                    <li><NavLink to='/glugpace/blog'>Blog</NavLink></li>
-                    <li><NavLink to='/glugpace/team'>Team</NavLink></li>
+                    <li className="nav-link"><NavLink to='/glugpace/events'>Events</NavLink></li>
+                    <li className="nav-link"><NavLink to='/glugpace/blog'>Blog</NavLink></li>
+                    <li className="nav-link"><NavLink to='/glugpace/team'>Team</NavLink></li>
                 </ul>
             </div>
         </header>
