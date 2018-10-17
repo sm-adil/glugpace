061de7a82c814e9ff34c1538f69d2cfd418eeecd
elmentrix <adilsheik98@gmail.com>
2018-10-17 20:48:06 +0530

    UPDATE: Updated member list and added avatar of team members

--- src/components/team/Member.js
+++ src/components/team/Member.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import memberpic from '../layout/images/logo.png'
+// import memberpic from '../layout/images/logo.png'
 
 const Member = ({members}) => {
   const memberList = members.map(member => {
     return (
         <div className="member-card" key={member.id}>
             <div className="member-details">
-                <img className="member-image" src={memberpic} alt="member" />
+                <img className="member-image" src={`https://avatars1.githubusercontent.com/${member.username}?size=100`} alt="member" />
                 <p className="member-name">{member.name}</p>
             </div>
         </div>
--- src/components/team/TeamList.js
+++ src/components/team/TeamList.js
@@ -4,19 +4,15 @@ import Member from './Member'
 class TeamList extends Component {
   state = {
       members: [
-          {id: '1', name: 'Ahmed Saifullah'},
-          {id: '2', name: 'Joythi Prakash'},
-          {id: '3', name: 'Nikhil Amin'},
-          {id: '4', name: 'Aditi Naik'},
-          {id: '5', name: 'Sheik Mohammed Adil'},
-          {id: '6', name: 'Suhail Afzal'},
-          {id: '7', name: 'Meem Sikandar Khan'},
-          {id: '8', name: 'Mohammed Salman Mohideen'},
-          {id: '9', name: 'Mohammed Shakir'},
-          {id: '10', name: 'Hasna Akbar Ali'},
-          {id: '11', name: 'Shahana Ankalgi'},
-          {id: '12', name: 'Saleh Arafath'},
-          {id: '13', name: 'Anagha Premraj'}
+          {id: '1', name: 'Sheik Mohammed Adil', username: 'elmentrix'},
+          {id: '2', name: 'Nikhil Amin', username: 'nikhilamin073'},
+          {id: '3', name: 'Aditi Naik', username: 'aditinaik'},
+          {id: '4', name: 'Ahmed Saifullah', username: 'Ahmed Saifullah'},
+          {id: '5', name: 'Mohammed Shakir', username: 'voidxyzvoidxyz'},
+          {id: '6', name: 'Hasna Akbar Ali', username: 'hasna-akbarali'},
+          {id: '7', name: 'Shahana Ankalgi', username: 'shahana98'},
+          {id: '8', name: 'Saleh Arafath', username: 'saleharafath'},
+          {id: '9', name: 'Anagha Premraj', username: 'anagha428'}
       ]
   }
   render() {
