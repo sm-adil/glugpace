110fc4fc6674651d2ea4a37dea169ab56e37884b
elmentrix <adilsheik98@gmail.com>
2018-10-20 14:24:41 +0530

    UPDATE: Changed date format

--- src/components/events/EventFile.js
+++ src/components/events/EventFile.js
@@ -4,7 +4,7 @@ module.exports = {
             {
                 "id": 1,
                 "title": "GIT & GITHUB 101",
-                "date": "27/10/2018",
+                "date": "27-10-2018",
                 "venue": "Unix Lab / Project Lab",
                 "body": "<p>The workshop will cover basics of using the Version Control System - Git and how to collaborate effectively on your projects using GitHub. Not just Git push and run but how to tackle difficult situations and how to find help. The workshop will also cover the benefits of being an open source contributor and how to be an open source contributor. We'll be introducing some exciting offers from GitHub education for students, and to use it to build your knowledge on modern technologies, and how open source can shape your career.</p>" +
                         "<b>This is a Free community driven workshop 💯</b>" + 
@@ -16,7 +16,7 @@ module.exports = {
             {
                 "id": 2,
                 "title": "IGNITE 19",
-                "date": "05/10/2018",
+                "date": "05-10-2018",
                 "venue": "Multi Purpose Hall",
                 "body": "<p>GLUG PACE is organizing IGNITE 19 on 5'th October 2018 for the students of second and third year B.E in P.A. College of engineering. The speaker of this event will be Mr. Manikanta A Nayar, who will be handling the session and guide the students very well about FOSS(Free and open source software).  And our beloved GLUG members gave a brief introduction about LINUX, FSMK, GLUG, free software and python. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.<p>",
                 "imageURl": "./event-images/ignite19.png"
@@ -24,7 +24,7 @@ module.exports = {
             {
                 "id": 3,
                 "title": "Laravel and PHP",
-                "date": "11/10/2017",
+                "date": "11-10-2017",
                 "venue": "Multi Purpose Hall",
                 "body": "<p>GLUG PACE organized Laravel and PHP workshop for the students of second and third year B.E in P.A. College of engineering. Chief guest of the program was Mr. Sushin P.V who handled the session and teach the students Laravel and PHP web frameworks. And our beloved GLUG members were the coordinators of this event. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.</p>",
                 "imageURl": "./event-images/laravel-php.jpg"
@@ -32,7 +32,7 @@ module.exports = {
             {
                 "id": 4,
                 "title": "IGNITE 16",
-                "date": "10/08/2016",
+                "date": "10-08-2016",
                 "venue": "Edu Sat Hall",
                 "body": "<p>GLUG PACE organized IGNITE 16 event on 10'th August 2016 for the students of second year B.E in P.A. College of engineering. Chief guest of the program was Mr. Shashank Chakravarthy who handled the session and guided the students very well about FOSS(Free and open source software). And our beloved GLUG members gave a brief introduction about LINUX, FSMK, GLUG, free software and python. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.</p>",
                 "imageURl": "./event-images/ignite16.jpg"
@@ -40,7 +40,7 @@ module.exports = {
             {
                 "id": 5,
                 "title": "Python Programming",
-                "date": "27/10/2016",
+                "date": "27-10-2016",
                 "venue": "Multi Purpose Hall",
                 "body": "<p>A intercollegiate workshop on python programming, organized by : GLUG PACE<p>",
                 "imageURl": "./event-images/python-workshop.jpg"
@@ -48,7 +48,7 @@ module.exports = {
             {
                 "id": 6,
                 "title": "Android Development",
-                "date": "05/08/2016",
+                "date": "05-08-2016",
                 "venue": "Project Lab",
                 "body": "<p>On behalf of GLUG PACE Mr. Sushin P.V took Android session for PACE students on 5'th and 12'th of August. The session started with introducing the concepts of Java and XML. Which continued with how to do the installation process. After the installation procedure, guided to get started with Android Studio. The session was for about an hour, which was really interesting. The session turned out to be a successful one. We hope to see further upcoming sessions from him, so that this would help students to build their technical skills.</p>",
                 "imageURl": "./event-images/android-session.jpg"
@@ -56,7 +56,7 @@ module.exports = {
             {
                 "id": 7,
                 "title": "Bytestruck 2015",
-                "date": "04/11/2015",
+                "date": "04-11-2015",
                 "venue": "Edu Sat Hall",
                 "body": "<p>On 4'th of Nov 2015, Department of Computer Science and Engineering PACE, conducted BYTESTRUCK-2015, a showcase event to explore the open software usefulness to the High school and PU students. This event was organized for spreading awareness of Free and Open Source Software (FOSS) among students and hence encourage them to use FOSS.</p>",
                 "imageURl": "./event-images/bytestruck2015.jpg"
