eeb393fd58df039ca26a231d767fa7f03c82037c
elmentrix <adilsheik98@gmail.com>
2018-10-20 15:52:43 +0530

    UPDATE: EventFile is updated with all new event details

--- src/components/events/EventFile.js
+++ src/components/events/EventFile.js
@@ -3,31 +3,27 @@ module.exports = {
         [
             {
                 "id": 1,
-                "title": "GIT & GITHUB 101",
-                "date": "27-10-2018",
-                "venue": "Unix Lab / Project Lab",
-                "body": "<p>The workshop will cover basics of using the Version Control System - Git and how to collaborate effectively on your projects using GitHub. Not just Git push and run but how to tackle difficult situations and how to find help. The workshop will also cover the benefits of being an open source contributor and how to be an open source contributor. We'll be introducing some exciting offers from GitHub education for students, and to use it to build your knowledge on modern technologies, and how open source can shape your career.</p>" +
-                        "<b>This is a Free community driven workshop 💯</b>" + 
-                        "<h2>Topics to be covered</h2>" +
-                        "<ul><li>What is Git & Why Use Git</li> <li>Installing Git</li> <li>How Git Works</li> <li>Creating a Repository</li> <li>Staging Files</li> <li>Making Commit</li> <li>Undoing Things</li> <li>Branches</li> <li>Merging Branches</li> <li>Introduction to GitHub</li> <li>Collaborating on GitHub</li> <li> Forking (& Contributing)</li> <li>Introduction to GitHub Pages</li></ul>" + 
-                        "<h3>Event Trainer: <span>Sheik Mohammed Adil</span></h3>",
-                "imageURl": "./event-images/github-session.jpg"
+                "title": "Bytestruck 2015",
+                "date": "04-11-2015",
+                "venue": "Edu Sat Hall",
+                "body": "<p>On 4'th of Nov 2015, Department of Computer Science and Engineering PACE, conducted BYTESTRUCK-2015, a showcase event to explore the open software usefulness to the High school and PU students. This event was organized for spreading awareness of Free and Open Source Software (FOSS) among students and hence encourage them to use FOSS.</p>",
+                "imageURl": "./event-images/bytestruck2015.jpg"
             },
             {
                 "id": 2,
-                "title": "IGNITE 19",
-                "date": "05-10-2018",
-                "venue": "Multi Purpose Hall",
-                "body": "<p>GLUG PACE is organizing IGNITE 19 on 5'th October 2018 for the students of second and third year B.E in P.A. College of engineering. The speaker of this event will be Mr. Manikanta A Nayar, who will be handling the session and guide the students very well about FOSS(Free and open source software).  And our beloved GLUG members gave a brief introduction about LINUX, FSMK, GLUG, free software and python. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.<p>",
-                "imageURl": "./event-images/ignite19.png"
+                "title": "Android Development",
+                "date": "05-08-2016",
+                "venue": "Project Lab",
+                "body": "<p>On behalf of GLUG PACE Mr. Sushin P.V took Android session for PACE students on 5'th and 12'th of August. The session started with introducing the concepts of Java and XML. Which continued with how to do the installation process. After the installation procedure, guided to get started with Android Studio. The session was for about an hour, which was really interesting. The session turned out to be a successful one. We hope to see further upcoming sessions from him, so that this would help students to build their technical skills.</p>",
+                "imageURl": "./event-images/android-session.jpg"
             },
             {
                 "id": 3,
-                "title": "Laravel and PHP",
-                "date": "11-10-2017",
+                "title": "Python Programming",
+                "date": "27-10-2016",
                 "venue": "Multi Purpose Hall",
-                "body": "<p>GLUG PACE organized Laravel and PHP workshop for the students of second and third year B.E in P.A. College of engineering. Chief guest of the program was Mr. Sushin P.V who handled the session and teach the students Laravel and PHP web frameworks. And our beloved GLUG members were the coordinators of this event. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.</p>",
-                "imageURl": "./event-images/laravel-php.jpg"
+                "body": "<p>A intercollegiate workshop on python programming, organized by : GLUG PACE<p>",
+                "imageURl": "./event-images/python-workshop.jpg"
             },
             {
                 "id": 4,
@@ -39,27 +35,36 @@ module.exports = {
             },
             {
                 "id": 5,
-                "title": "Python Programming",
-                "date": "27-10-2016",
+                "title": "Laravel and PHP",
+                "date": "11-10-2017",
                 "venue": "Multi Purpose Hall",
-                "body": "<p>A intercollegiate workshop on python programming, organized by : GLUG PACE<p>",
-                "imageURl": "./event-images/python-workshop.jpg"
+                "body": "<p>GLUG PACE organized Laravel and PHP workshop for the students of second and third year B.E in P.A. College of engineering. Chief guest of the program was Mr. Sushin P.V who handled the session and teach the students Laravel and PHP web frameworks. And our beloved GLUG members were the coordinators of this event. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.</p>",
+                "imageURl": "./event-images/laravel-php.jpg"
             },
             {
                 "id": 6,
-                "title": "Android Development",
-                "date": "05-08-2016",
-                "venue": "Project Lab",
-                "body": "<p>On behalf of GLUG PACE Mr. Sushin P.V took Android session for PACE students on 5'th and 12'th of August. The session started with introducing the concepts of Java and XML. Which continued with how to do the installation process. After the installation procedure, guided to get started with Android Studio. The session was for about an hour, which was really interesting. The session turned out to be a successful one. We hope to see further upcoming sessions from him, so that this would help students to build their technical skills.</p>",
-                "imageURl": "./event-images/android-session.jpg"
+                "title": "IGNITE 19",
+                "date": "05-10-2018",
+                "venue": "Multi Purpose Hall",
+                "body": "<p>GLUG PACE is organizing IGNITE 19 on 5'th October 2018 for the students of second and third year B.E in P.A. College of engineering. The speaker of this event will be Mr. Manikanta A Nayar, who will be handling the session and guide the students very well about FOSS(Free and open source software).  And our beloved GLUG members gave a brief introduction about LINUX, FSMK, GLUG, free software and python. Overall the event was successfully conducted with a great support from our juniors, staff coordinators and the GLUG PACE members.<p>" + 
+                "<p>The event started with a brief introduction to free and open source software by Manikanta. He explained about the importance of open source software development in the current generation which was quiet inspirational. It was then followed by a feedback of the previous workshop conducted on Laravel and PHP under GLUG, given by Shahana Ankalgi. The current president of GLUG PACE, Sheik Mohammed Adil introduced the website developed by himself with the design help of SOSC team's website. There was also an interesting session of Group Discussion which included the 2nd year students to find a problem that everyone faces along with the solution. The event was concluded by introducing the core members of GLUG PACE.</p>",
+                "imageURl": "./event-images/ignite19.png"
             },
             {
                 "id": 7,
-                "title": "Bytestruck 2015",
-                "date": "04-11-2015",
-                "venue": "Edu Sat Hall",
-                "body": "<p>On 4'th of Nov 2015, Department of Computer Science and Engineering PACE, conducted BYTESTRUCK-2015, a showcase event to explore the open software usefulness to the High school and PU students. This event was organized for spreading awareness of Free and Open Source Software (FOSS) among students and hence encourage them to use FOSS.</p>",
-                "imageURl": "./event-images/bytestruck2015.jpg"
-            },
+                "title": "GIT & GITHUB 101",
+                "date": "27-10-2018",
+                "venue": "Unix Lab / Project Lab",
+                "body": "<p>The workshop will cover basics of using the Version Control System - Git and how to collaborate effectively on your projects using GitHub. Not just Git push and run but how to tackle difficult situations and how to find help. The workshop will also cover the benefits of being an open source contributor and how to be an open source contributor. We'll be introducing some exciting offers from GitHub education for students, and to use it to build your knowledge on modern technologies, and how open source can shape your career.</p>" +
+                        "<b>This is a Free community driven workshop 💯</b>" + 
+                        "<h2>Topics to be covered</h2>" +
+                        "<ul><li>What is Git & Why Use Git</li> <li>Installing Git</li> <li>How Git Works</li> <li>Creating a Repository</li> <li>Staging Files</li> <li>Making Commit</li> <li>Undoing Things</li> <li>Branches</li> <li>Merging Branches</li> <li>Introduction to GitHub</li> <li>Collaborating on GitHub</li> <li> Forking (& Contributing)</li> <li>Introduction to GitHub Pages</li></ul>" + 
+                        "<h3>Event Trainer's: </h3>" +
+                        "<ul><li>Musthaq Ahamad, GitHub Campus Expert | Community Lead - SOSC</li> <li>We'll also have few more SOSC Community Lead's joining us!</li></ul>" + 
+                        "<h3>Prerequisite</h3>" + 
+                        "<ul> <li><a href=\"https://github.com\" target=\"blank\">Github Account</a> - Use/Add your college Email ID to your account.</li> <li>Laptop Fully Charged (plug points will be available)</li> <li><a href=\"https://git-scm.com/download/win\" target=\"blank\">Git Bash</a> installed if you are on Windows</li> <li>Git installed and checked on Linux/Mac Laptops.</li></ul>" +
+                        "<h3>Registration Link: <a href=\"https://goo.gl/forms/MKIIpJPQ4Y5GqVAv1\" target=\"blank\">Register</a></h3>",
+                "imageURl": "./event-images/github-session.jpg"
+            }
         ]
 }
